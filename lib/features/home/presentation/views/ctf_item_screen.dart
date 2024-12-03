6b5d39bb8e45cb92c77b40aee4d23d4059455a75
Mahmoudeldeeb1000 <mahmoud.eldeeb.16371@gmail.com>
2024-12-03 19:00:46 +0200

    start ctf screen

---
+++ lib/features/home/presentation/views/ctf_item_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:spider/features/home/presentation/views/widgets/ctf_item_body.dart';
+
+class CtfItemScreen extends StatelessWidget {
+  const CtfItemScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CtfItemBody();
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/ctf_item_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CtfItemBody extends StatelessWidget {
+  const CtfItemBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
